fdc9e06e4b6772571cb6953b09c80aaf3cb42468
Kyrylo Silin <silin@kyrylo.org>
2015-06-24 20:00:49 +0300

    Refactor

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+
+		watch: {
+			files: 'assets/js/**/*.js',
+      tasks: ['concat']
+		},
+
+		concat: {
+      options: {
+        banner: "'use strict';\n",
+        process: function(src, filepath) {
+          return '// Source: ' + filepath + '\n' +
+            src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+        }
+      },
+
+      dist: {
+        src: [
+					'node_modules/angular/angular.js',
+					'node_modules/angular-route/angular-route.js',
+          'assets/js/main.js'
+        ],
+
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    }
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['concat', 'watch']);
+};
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,87 +1,170 @@
-require('angular');
+/*global angular*/
+var net = require('net'),
+    events = require('events');
 
-var net = require('net');
+var dependencies = [
+  'ngRoute'
+];
 
-var client = new net.Socket();
-var gem_count = 0;
-var behavior_count = 0;
+var systemBrowser = angular.module('systemBrowser', dependencies);
 
-client.connect(9696, '127.0.0.1', function() {
-	console.log('Connected');
+systemBrowser.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/',
+          {
+            controller: 'MainController',
+            templateUrl: 'assets/partials/main_view.html'
+          })
+    .otherwise({redirectTo: '/'});
+}]);
+
+systemBrowser.factory('emitter', function() {
+  return new events.EventEmitter();
 });
 
-client.on('data', function(data) {
-	var raw_messages = data.toString().split('\n').filter(function(elem) {
-		return elem === 0 || elem;
-	});
-	var messages = [];
+systemBrowser.factory('socket', function() {
+  var socket = new net.Socket();
 
-	raw_messages.forEach(function(raw_message) {
-		messages.push(JSON.parse(raw_message));
-	});
+  socket.connect(9696, '127.0.0.1', function() {
+    console.log('Connected to 127.0.0.1:9696');
+  });
 
-	messages.forEach(function(message) {
-		var payload = message.payload;
-
-		switch (payload.command) {
-		case 'ADD_GEM':
-			var el = document.querySelector('#gem-list ul');
-			payload.data.forEach(function(datum) {
-				var li = document.createElement("li");
-
-				if (gem_count++ % 2 === 0) {
-					li.className = 'odd';
-				}
-
-				li.innerHTML = datum;
-				li.onclick = function() {
-					client.write('GET_BEHAVIORS ' + this.innerHTML + '\n');
-				};
-
-				el.appendChild(li);
-			});
-
-			break;
-		case 'ADD_BEHAVIOR':
-			var el = document.querySelector('#behavior-list ul');
-			payload.data.forEach(function(datum) {
-				var li = document.createElement("li");
-
-				if (behavior_count++ % 2 === 0) {
-					li.className = 'odd';
-				}
-
-				li.innerHTML = datum;
-				li.onclick = function() {
-					client.write('GET_METHODS ' + this.innerHTML + '\n');
-				};
-
-				el.appendChild(li);
-			});
-
-			break;
-		case 'ADD_METHOD':
-			var el = document.querySelector('#method-list ul');
-			payload.data.forEach(function(datum) {
-				var li = document.createElement("li");
-
-				if (behavior_count++ % 2 === 0) {
-					li.className = 'odd';
-				}
-
-				li.innerHTML = datum;
-				li.onclick = function() {
-					client.write('SHOW_SOURCE ' + this.innerHTML + '\n');
-				};
-
-				el.appendChild(li);
-			});
-
-			break;
-		}
-	});
+  return socket;
 });
 
-client.on('close', function() {
-	console.log('Connection closed');
-});
+var Request = function(action, resource, scope) {
+  this.body = {
+    system_browser_server: {
+      action: action,
+      resource: resource,
+      scope: scope
+    }
+  };
+};
+Request.prototype.constructor = Request;
+
+Request.prototype.to_json = function() {
+  return JSON.stringify(this.body) + '\n';
+};
+
+systemBrowser
+  .factory('gemFactory', ['socket', function(socket) {
+  return {
+    getGems: function() {
+      var req = new Request('get', 'gem', 'all');
+      socket.write(req.to_json());
+    }
+  };
+}]);
+
+systemBrowser
+  .factory('behaviourFactory', ['socket', function(socket) {
+  return {
+    getBehaviours: function(gem_name) {
+      var req = new Request('get', 'behaviour', gem_name);
+      socket.write(req.to_json());
+    }
+  };
+}]);
+
+var GemController = function($scope, emitter, gemFactory) {
+  emitter.on('get:gem:all', function() {
+    gemFactory.getGems();
+  });
+
+  emitter.on('add:gem:all', function(gems) {
+    var ruby_gems = gems.slice(2, gems.length);
+    var core_gems = gems.slice(0, 2);
+
+    $scope.items = core_gems.concat(ruby_gems.sort());
+  });
+
+  $scope.getSublist = function(gem_name) {
+    emitter.emit('get:behaviour:all', gem_name);
+  };
+};
+
+var BehaviourController = function($scope, emitter, behaviourFactory) {
+  emitter.on('get:behaviour:all', function(gem_name) {
+    emitter.once('add:behaviour:' + gem_name, function(behaviours) {
+      if (behaviours.length === 0) {
+        $scope.$apply(function() {
+          $scope.items = ['No behaviours found'];
+        });
+      } else {
+        $scope.$apply(function() {
+          $scope.items = behaviours;
+        });
+      }
+    });
+
+    behaviourFactory.getBehaviours(gem_name);
+  });
+};
+
+var GroupController = function($scope) {
+};
+
+var MethodController = function($scope) {
+};
+
+var MessageDispatcher = function() {
+};
+MessageDispatcher.prototype.constructor = MessageDispatcher;
+
+MessageDispatcher.prototype.dispatch = function(data) {
+  var dataArray, messages;
+
+  dataArray = data.toString().split('\n');
+
+  messages = dataArray.filter(function(elem) {
+    return elem === 0 || elem;
+  });
+
+  return messages.map(function(message) {
+    var msg = JSON.parse(message);
+    return msg.system_browser_client;
+  });
+};
+
+var EventLoop = function(socket, emitter) {
+  this.socket = socket;
+  this.emitter = emitter;
+};
+EventLoop.prototype.constructor = EventLoop;
+
+EventLoop.prototype.init = function() {
+  var that = this;
+
+  return function(data) {
+    var messages = new MessageDispatcher().dispatch(data);
+
+    messages.forEach(function(message) {
+      this.emitter.emit(message.action, message.data);
+    }, that);
+  };
+};
+
+systemBrowser.controller('GemController',
+                         ['$scope', 'emitter', 'gemFactory', GemController]);
+systemBrowser.controller('BehaviourController',
+                         ['$scope', 'emitter', 'behaviourFactory', BehaviourController]);
+systemBrowser.controller('GroupController', ['$scope', GroupController]);
+systemBrowser.controller('MethodController', ['$scope', MethodController]);
+
+systemBrowser
+  .controller('MainController', ['$scope', 'socket', 'emitter',
+    function ($scope, socket, emitter, gemFactory) {
+  $scope.panes = [
+    {name: 'gem', controller: GemController},
+    {name: 'behaviour', controller: BehaviourController},
+    {name: 'group', controller: GroupController},
+    {name: 'method', controller: MethodController}
+  ];
+
+  socket.on('data', new EventLoop(socket, emitter).init());
+
+  emitter.on('init', function() {
+    emitter.emit('get:gem:all');
+  });
+}]);

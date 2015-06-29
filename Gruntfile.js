3f1ece35b8ae0b1de428a5a5f223eeda4c10d084
Kyrylo Silin <silin@kyrylo.org>
2015-06-29 14:39:36 +0300

    Export CSS and fonts to dist/

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
+    dist: 'dist/',
 
     watch: {
       files: [
@@ -31,13 +32,44 @@ module.exports = function(grunt) {
           'assets/js/main.js'
         ],
 
-        dest: 'dist/<%= pkg.name %>.js'
+        dest: '<%= dist %><%= pkg.name %>.js'
+      }
+    },
+
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+
+      target: {
+        files: {
+          '<%= dist %><%= pkg.name %>.css': [
+            'node_modules/normalize.css/normalize.css',
+            'bower_components/angular-ui-layout/ui-layout.css',
+            'assets/css/main.css'
+          ]
+        }
+      }
+    },
+
+    copy: {
+      main: {
+        files: [
+          {expand: true,
+           flatten: true,
+           src: ['assets/fonts/*'],
+           dest: '<%= dist %>',
+           filter: 'isFile'},
+        ]
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', ['concat', 'watch']);
+  grunt.registerTask('default', ['concat', 'cssmin', 'copy', 'watch']);
 };

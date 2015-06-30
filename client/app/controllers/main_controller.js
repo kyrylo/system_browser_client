7812353fb74305f40c062af4540bee126dfa5431
Kyrylo Silin <silin@kyrylo.org>
2015-06-30 17:10:13 +0300

    main_controller: delete dead code

--- client/app/controllers/main_controller.js
+++ client/app/controllers/main_controller.js
@@ -3,34 +3,7 @@
 (function(global) {
   'use strict';
 
-  var controller = function($scope, $controller, socket, emitter, Gem,
-                            EventLoop) {
-    var gem = $scope.$new(),
-        behaviour = $scope.$new(),
-        group = $scope.$new(),
-        method = $scope.$new();
-
-    var GemCtrl = new Function(),
-        BehaviourCtrl = new Function(),
-        GroupCtrl = new Function(),
-        MethodCtrl = new Function();
-
-    GemCtrl.prototype = gem;
-    BehaviourCtrl.prototype = behaviour;
-    GroupCtrl.prototype = group;
-    MethodCtrl.prototype = method;
-
-    $controller('GemController', {$scope: gem});
-    $controller('BehaviourController', {$scope: behaviour});
-    $controller('GroupController', {$scope: group});
-    $controller('MethodController', {$scope: method});
-
-    $scope.panes = [
-      {name: 'gem', controller: GemCtrl},
-      {name: 'behaviour', controller: BehaviourCtrl},
-      {name: 'group', controller: GroupCtrl},
-      {name: 'method', controller: MethodCtrl}
-    ];
+  var controller = function($scope, socket, emitter, EventLoop) {
 
     socket.on('data', EventLoop.init());
 
@@ -41,10 +14,8 @@
 
   global.app.controller('MainController', [
     '$scope',
-    '$controller',
     'socket',
     'emitter',
-    'Gem',
     'EventLoop',
     controller]);
 })(window.global);

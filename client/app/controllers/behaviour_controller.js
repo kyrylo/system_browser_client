7a8ee6162c2a89f3c5ff0bfd5d80365001007556
Kyrylo Silin <silin@kyrylo.org>
2015-07-02 15:56:56 +0300

    Add support for nested classes

--- client/app/controllers/behaviour_controller.js
+++ client/app/controllers/behaviour_controller.js
@@ -3,7 +3,7 @@
 
   var _ = require('underscore');
 
-  var controller = function($scope, Behaviour) {
+  var controller = function($scope, $sce, Behaviour) {
     $scope.items = [];
 
     $scope.$on('get:behaviour:all', function(_event1, gem) {
@@ -20,10 +20,32 @@
             });
           } else {
             $scope.$apply(function() {
-              $scope.items = _.sortBy(behaviours, 'name').map(function(behaviour) {
+              var sortedBehaviours = _.sortBy(behaviours, 'name').map(function(behaviour) {
                 behaviour.selected = false;
                 return behaviour;
               });
+
+              var behaviourTree = sortedBehaviours.map(function(behaviour) {
+                if (behaviour.name) {
+                  // A feeble attempt at supporting behaviours
+                  // with redefined #name
+                  if (/^[^A-Z]/.test(behaviour.name)) {
+                    behaviour.displayName = behaviour.name;
+                    return behaviour;
+                  }
+
+                  var nestedClasses = behaviour.name.split('::'),
+                      nestedClassesCount = nestedClasses.length,
+                      className = nestedClasses[nestedClassesCount - 1],
+                      indent = new Array(nestedClassesCount).join(' &bull; ');
+
+                  behaviour.indent = $sce.trustAsHtml(indent);
+                  behaviour.displayName = className;
+                }
+                return behaviour;
+              });
+
+              $scope.items = behaviourTree;
             });
           }
         });
@@ -42,14 +64,32 @@
       $scope.$parent.$broadcast('get:method:all', behaviour);
     };
 
-    $scope.select = function(behaviour) {
+    $scope.select = function($event, behaviour) {
       $scope.$emit('list-box:behaviour:selected');
       behaviour.selected = true;
+
+      $scope.items.forEach(function(behaviour) {
+        $scope.hideIndent(behaviour);
+      });
+
+      $scope.showIndent(behaviour);
+    };
+
+    $scope.showIndent = function(behaviour) {
+      behaviour.visibleIndent = true;
+    };
+
+    $scope.hideIndent = function(behaviour) {
+      if (behaviour.selected) {
+        return;
+      }
+      behaviour.visibleIndent = false;
     };
   };
 
   global.app.controller('BehaviourController', [
     '$scope',
+    '$sce',
     'Behaviour',
     controller]);
 })(window.global);

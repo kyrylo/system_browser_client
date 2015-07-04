b003fffde851441cfd234c52f4cfab746f4aed5e
Kyrylo Silin <silin@kyrylo.org>
2015-07-04 18:05:10 +0300

    app.behaviour: refactor the controller

--- client/app/modules/app.behaviour/controllers/behaviour_controller.js
+++ client/app/modules/app.behaviour/controllers/behaviour_controller.js
@@ -2,69 +2,102 @@
   'use strict';
 
   var controller = function($scope, $rootScope, $sce, Behaviour, _) {
+    // --------------------------- Public variables ----------------------------
     $scope.behaviours = [];
 
+    // --------------------------- Events --------------------------------------
+
     $scope.$on('get:behaviour:all', function(_event1, gem) {
-      var name = 'add:behaviour:' + gem.name;
+      var event = 'add:behaviour:' + gem.name;
 
-      if (!$scope.$$listeners.hasOwnProperty(name)) {
-        $scope.$on(name, function(_event2, behaviours) {
-          $scope.$parent.$broadcast('reset-methods');
-          $scope.$parent.$broadcast('reset-source');
-
-          if (behaviours.length === 0) {
-            $scope.$apply(function() {
-              $scope.behaviours = [{displayName: 'No behaviours found'}];
-            });
-          } else {
-            $scope.$apply(function() {
-              var sortedBehaviours = _.sortBy(behaviours, 'name').map(function(behaviour) {
-                behaviour.selected = false;
-                return behaviour;
-              });
-
-              var behaviourTree = sortedBehaviours.map(function(behaviour) {
-                if (behaviour.isModule) {
-                  behaviour.icon = 'module';
-                } else if (behaviour.isException) {
-                  behaviour.icon = 'exception';
-                } else {
-                  behaviour.icon = 'class';
-                }
-
-                if (behaviour.name) {
-                  // A feeble attempt at supporting behaviours
-                  // with redefined #name
-                  if (/^[^A-Z]/.test(behaviour.name)) {
-                    behaviour.displayName = behaviour.name;
-                    return behaviour;
-                  }
-
-                  var nestedClasses = behaviour.name.split('::'),
-                      nestedClassesCount = nestedClasses.length,
-                      className = nestedClasses[nestedClassesCount - 1],
-                      indent = new Array(nestedClassesCount).join(' &bull; ');
-
-                  behaviour.indentation = $sce.trustAsHtml(indent);
-                  behaviour.displayName = className;
-                }
-                return behaviour;
-              });
-
-              $scope.behaviours = behaviourTree;
-            });
-          }
-        });
+      if (!$scope.$$listeners.hasOwnProperty(event)) {
+        listenTo(event);
       }
 
       Behaviour.getAllFrom(gem.name);
     });
 
     $scope.$on('list-box:behaviour:selected', function() {
-      $scope.behaviours.forEach(function(behaviour) {
-        behaviour.selected = false;
-      });
+      $scope.behaviours.forEach(deselectBehaviour);
     });
+
+    // --------------------------- Private methods -----------------------------
+
+    var resetMethodState = function() {
+      $scope.$parent.$broadcast('reset-methods');
+      $scope.$parent.$broadcast('reset-source');
+    };
+
+    var listenTo = function(event) {
+      $scope.$on(event, function(_event2, behaviours) {
+        resetMethodState();
+
+        if (behaviours.length === 0) {
+          $scope.$apply(function() {
+            $scope.behaviours = [{displayName: 'No behaviours found'}];
+          });
+        } else {
+          $scope.$apply(function() {
+            var sortedBehaviours = _.sortBy(behaviours, 'name').map(function(behaviour) {
+              behaviour.selected = false;
+              return behaviour;
+            });
+
+            var behaviourTree = sortedBehaviours.map(function(behaviour) {
+              if (behaviour.isModule) {
+                behaviour.icon = 'module';
+              } else if (behaviour.isException) {
+                behaviour.icon = 'exception';
+              } else {
+                behaviour.icon = 'class';
+              }
+
+              if (behaviour.name) {
+                // A feeble attempt at supporting behaviours
+                // with redefined #name
+                if (/^[^A-Z]/.test(behaviour.name)) {
+                  behaviour.displayName = behaviour.name;
+                  return behaviour;
+                }
+
+                var nestedClasses = behaviour.name.split('::'),
+                    nestedClassesCount = nestedClasses.length,
+                    className = nestedClasses[nestedClassesCount - 1],
+                    indent = new Array(nestedClassesCount).join(' &bull; ');
+
+                behaviour.indentation = $sce.trustAsHtml(indent);
+                behaviour.displayName = className;
+              }
+              return behaviour;
+            });
+
+            $scope.behaviours = behaviourTree;
+          });
+        }
+      });
+    };
+
+    var selectBehaviour = function(behaviour) {
+      behaviour.selected = true;
+    };
+
+    var deselectBehaviour = function(behaviour) {
+      behaviour.selected = false;
+    };
+
+    var hideIndentation = function(behaviour) {
+      if (behaviour.selected) {
+        return;
+      }
+
+      behaviour.visibleIndentation = false;
+    };
+
+    var showIndentation = function(behaviour) {
+      behaviour.visibleIndentation = true;
+    };
+
+    // --------------------------- Public methods ------------------------------
 
     $scope.showGroups = function(behaviour) {
       $rootScope.$broadcast('get:method:all', behaviour);
@@ -73,26 +106,15 @@
 
     $scope.selectBehaviour = function($event, behaviour) {
       $scope.$emit('list-box:behaviour:selected');
-      behaviour.selected = true;
+      selectBehaviour(behaviour);
 
-      $scope.behaviours.forEach(function(behaviour) {
-        $scope.hideIndentation(behaviour);
-      });
-
-      $scope.showIndentation(behaviour);
+      $scope.behaviours.forEach(hideIndentation);
+      showIndentation(behaviour);
     };
 
-    $scope.showIndentation = function(behaviour) {
-      behaviour.visibleIndentation = true;
-    };
+    $scope.showIndentation = showIndentation;
 
-    $scope.hideIndentation = function(behaviour) {
-      if (behaviour.selected) {
-        return;
-      }
-
-      behaviour.visibleIndentation = false;
-    };
+    $scope.hideIndentation = hideIndentation;
   };
 
   global.app.controller('BehaviourController', [
